9fe275ca59e796abe82f9611f7be325e02ddefb5
Theo Nam Truong <theotr@amazon.com>
2022-12-01 12:49:23 -0700

    Version bump for dsl and transport in preparation for release (#125)
    Signed-off-by: Theo Truong <theotr@amazon.com>

--- opensearch-dsl/lib/opensearch/dsl/version.rb
+++ opensearch-dsl/lib/opensearch/dsl/version.rb
@@ -26,6 +26,6 @@
 
 module OpenSearch
   module DSL
-    VERSION = "0.2.0"
+    VERSION = "0.2.1"
   end
 end
--- opensearch-transport/lib/opensearch/transport/version.rb
+++ opensearch-transport/lib/opensearch/transport/version.rb
@@ -26,6 +26,6 @@
 
 module OpenSearch
   module Transport
-    VERSION = '2.0.1'.freeze
+    VERSION = '2.1.0'.freeze
   end
 end

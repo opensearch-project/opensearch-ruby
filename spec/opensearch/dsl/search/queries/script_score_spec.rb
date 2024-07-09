dd43533a8836d2e4fc351c8dbd4a4ec7d9e13af3
Kim Quang Cap <kimquangcap@gmail.com>
2024-07-09 15:25:34 +0700

    Add license header for OS.
    Signed-off-by: Kim Quang Cap <kimquangcap@gmail.com>

--- spec/opensearch/dsl/search/queries/script_score_spec.rb
+++ spec/opensearch/dsl/search/queries/script_score_spec.rb
@@ -1,3 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+
 require_relative '../../../../spec_helper'
 
 describe OpenSearch::DSL::Search::Queries::ScriptScore do
